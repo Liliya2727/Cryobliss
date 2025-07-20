c1b5fbebfbcc3dcbfe74eaa99d5e83671954bb59
ネコ Liliya <syarifbintang16@gmail.com>
2025-07-20 23:57:15 +0700

    Fix undefined symbol errors by ensuring all utility functions are linked and headers included

--- jni/Android.mk
+++ jni/Android.mk
@@ -3,11 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := Cryx
 LOCAL_SRC_FILES := \
+    src/freq.c \
     main.c \
     src/cmd_utils.c \
     src/Cryx_log.c \
     src/file_utils.c \
-    src/freq.c \
     src/misc_utils.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
